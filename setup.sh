61601a69364205d4f94bd22d207b33288872fb44
Christina Bukas <31160776+christinab12@users.noreply.github.com>
2021-08-20 17:25:16 +0200

    Delete setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-conda create -n fnet python=3.7
-conda activate fnet
-pip install -r requirements.txt
-conda install -c pytorch pytorch=1.8.0 torchvision=0.9.0 cuda91=1.0
-./scripts/test_run.sh
